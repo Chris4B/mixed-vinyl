ba2e4fb04fdd0bb26aa4e5e61273d07b614d9996
Chris Balla <ballachris13@gmail.com>
2023-01-29 17:53:47 +0100

    first syntax twig and communication with controller

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,14 +4,25 @@ namespace App\Controller;
 
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     public function homepage():Response
     {
-        return new Response('je sui content');
+        $tracks = [
+            ['song'=>'Gansta Paradise', 'artist' =>'Coolio'],
+            ['song'=>'Waterfalls', 'artist' =>'TLC'],
+            ['song'=>'Creep', 'artist' =>'Radiohead'],
+            ['song'=>'kiss from a Rose', 'artist' =>'Boys II Men'],
+            ['song'=>'Fantasy', 'artist' =>'Mariah Carey'],
+        ];
+        return $this->render('Vinyl/homepage.html.twig',[
+            'title' =>'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
      public function browse(string $slug = null):Response
      {
