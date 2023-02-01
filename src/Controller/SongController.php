f1b9196a7a759ac9bfba5ab611a45089a614ac74
Chris Balla <ballachris13@gmail.com>
2023-02-01 08:17:27 +0100

    services Objects, logger interface object

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class SongController extends AbstractController
 {
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
         $song = [
@@ -16,6 +17,10 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('returning API response for song {song}',[
+            'song'=>$id,
+        ]);
 
        // return new JsonResponse($song);
           return $this->json($song);
