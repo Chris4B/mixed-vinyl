58440b8a0ca7613ef528ba2137ca0b84113b962a
Chris Balla <ballachris13@gmail.com>
2023-02-01 07:45:13 +0100

    update file

---
+++ src/Controller/SongController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class SongController extends AbstractController
+{
+    public function getSong($id): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+       // return new JsonResponse($song);
+          return $this->json($song);
+    }
+}
